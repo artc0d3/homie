ab7dd4f60e6fe34baea8b80ff50686370c605d2a
Artur Markiewicz <artur.m@posteo.de>
2026-05-14 19:40:21 +0000

    feat: Cockpit service added

---
+++ services/cockpit.nix
@@ -0,0 +1,9 @@
+# Cockpit is a web-based server management tool that allows you to manage your server through a web interface.
+{ ... }:
+{
+  services.cockpit = {
+    enable = true;
+    port = 9090;
+    openFirewall = true;
+  };
+}
